e424ce00c2b693c42138037f6b3595c22cf4a005
Godfrey Nolan <godfrey@riis.com>
2016-10-17 20:30:50 -0400

    Initial Commit

---
+++ HelloWorldCuckoo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorldCuckoo
+//
+//  Created by User on 10/17/16.
+//  Copyright © 2016 riis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

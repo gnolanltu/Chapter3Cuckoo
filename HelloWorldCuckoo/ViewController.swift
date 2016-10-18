3ecaace81f9b2be815af6e749ed3faab4e650a91
Godfrey Nolan <godfrey@riis.com>
2016-10-17 20:42:37 -0400

    added model

--- HelloWorldCuckoo/ViewController.swift
+++ HelloWorldCuckoo/ViewController.swift
@@ -11,8 +11,11 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var labelText: UILabel!
+    let hw = HelloWorld()
+    
     @IBAction func updateLabel(_ sender: UIButton) {
-        labelText.text = "Hello World"
+        // labelText.text = "Hello World"
+        labelText.text = hw.message(msg: "Hello World")
     }
     override func viewDidLoad() {
         super.viewDidLoad()

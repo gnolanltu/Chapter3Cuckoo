16c02712e2a5f6e0453a502a904c65a8497f2c8a
Godfrey Nolan <godfrey@riis.com>
2016-10-17 20:34:11 -0400

    initial

--- HelloWorldCuckoo/ViewController.swift
+++ HelloWorldCuckoo/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelText: UILabel!
+    @IBAction func updateLabel(_ sender: UIButton) {
+        labelText.text = "Hello World"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
